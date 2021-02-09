01f4c0cdc11b926d36bb8b9e3fd816626350f955
nickwill24 <nick2428.nw@gmail.com>
2021-02-09 13:25:32 -0500

    complete place dice button

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 console.log("Welcome to Dragon Ball Z Sayian Board Game")
 
-
-//let gameActive: true; 
-//let currentPlayer: goku;
-
-
+let counter = 0 
+let gameActive= true; 
+let PlayerOne = "goku";
+let playerTwo = "vegeta"
+let winSpotCombo = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" , "" ]
+let gameDice = 0 
 //make the player character move up, down, and back 
 //display the dice and how many move the player move 
 // make bad spot and good spot work 
@@ -21,10 +22,24 @@ console.log("Welcome to Dragon Ball Z Sayian Board Game")
 
 
 
+    //function rollTheDice() { 
+       // setTimeout(function () { 
+           // let randomNumber1 = Math.floor(Math.random() * 6) + 1; 
+            //let randomNumber2 = Math.floor(Math.random() * 6) + 1; 
+
+            //document.querySelector(".img1").setAttribute("src", 
+            //    "dice" + randomNumber1 + ".png"); 
+
+            //document.querySelector(".img2").setAttribute("src", 
+              //  "dice" + randomNumber2 + ".png"); 
+
+            //if (randomNumber1 === randomNumber2) { 
+              //  document.querySelector("h1").innerHTML = "Draw!"; 
+            //} 
 
 
 
     //document.querySelectorAll('.cell').forEach(function(cell) {
      //   cell.addEventListener('click', handleClick);
     //})
-   // document.querySelector('.reset').addEventListener('click', reset)
+//document.querySelector('.reset').addEventListener('click', reset)
