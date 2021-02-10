b0db28077ed513f34f108e73ea67d59e45da62ef
nickwill24 <nick2428.nw@gmail.com>
2021-02-09 21:49:28 -0500

    add the game pieces movement extend

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ let counter = 0
 let gameActive= true; 
 let dice = document.querySelector(".dice") 
 let gokuPicture = "images/Goku.png"
+let vegetaPicture = "images/"
 const startSquare = document.querySelector("#start")
 let gamePieceOne = document.createElement("img") 
 gamePieceOne.src = gokuPicture
 gamePieceOne.className = "gamePiece"
-gameBoard = ['start', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
+gameBoard = ['start', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twleve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eigthteen', 'nineteen', 'twenty', 'twenty-one','twenty-two', 'twenty-three', 'twenty-four', 'twenty-five', 'twenty-six', 'twenty-seven', 'twenty-eight', 'twenty-nine', 'thirty', 'thirty-one', 'thirty-two', 'thirty-three', 'thirty-four', 'thirty-five', 'thirty-six', 'thirty-seven']
 
 const playGame = () => {
 startSquare.appendChild(gamePieceOne)
