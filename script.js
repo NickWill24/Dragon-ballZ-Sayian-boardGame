d8e6c92eb6ef46d70ff5b9bbbfe6e6ec5962b05a
nickwill24 <nick2428.nw@gmail.com>
2021-02-09 19:23:15 -0500

    complete the dice function

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ let counter = 0
 let gameActive= true; 
 let PlayerOne = "goku";
 let playerTwo = "vegeta"
-let winSpotCombo = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" , "" ]
-let gameDice = 0 
+let dice = document.querySelector(".dice") 
 //make the player character move up, down, and back 
 //display the dice and how many move the player move 
 // make bad spot and good spot work 
@@ -14,32 +13,21 @@ let gameDice = 0
 //let gameStatus= document.querySelector('.gameStatus')
 //gameStatus.innerHTML=`player turn: ${currentPlayer}`
 
-
+//const winningContidonal 
 //const handleClick= function(event){
-    //console.log(event.target.id)
-    //const clickCell= event.target
-    //const gamestateIndex= parseInt(clickCell.id)
 
 
 
-    //function rollTheDice() { 
-       // setTimeout(function () { 
-           // let randomNumber1 = Math.floor(Math.random() * 6) + 1; 
-            //let randomNumber2 = Math.floor(Math.random() * 6) + 1; 
-
-            //document.querySelector(".img1").setAttribute("src", 
-            //    "dice" + randomNumber1 + ".png"); 
-
-            //document.querySelector(".img2").setAttribute("src", 
-              //  "dice" + randomNumber2 + ".png"); 
-
-            //if (randomNumber1 === randomNumber2) { 
-              //  document.querySelector("h1").innerHTML = "Draw!"; 
-            //} 
+    const rollTheDice = () => { 
+        setTimeout(function () { 
+        let diceValue = Math.floor(Math.random() * 6) + 1; 
+        console.log(diceValue)
+        dice.src = `./images/dice${diceValue}.png`      
+        }, 500) 
+    }
 
 
 
-    //document.querySelectorAll('.cell').forEach(function(cell) {
-     //   cell.addEventListener('click', handleClick);
-    //})
 //document.querySelector('.reset').addEventListener('click', reset)
+
+dice.addEventListener('click',rollTheDice)
